ba0609c742cd8dd01a8321a53b7a5a2e45d7f99b
leenaabdalrahman11 <leenasba02@gmail.com>
2025-10-29 11:45:26 +0200

    add auth.js

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -2,6 +2,8 @@ import {Router} from 'express';
 import { auth } from '../../middleware/auth.js';
 import * as controller from './category.controller.js'
 const router = Router();
-router.post('/',auth(),controller.create);
+
+router.post('/',auth(['admin']),controller.create);
+router.get('/',auth(['user','admin']),controller.get);
 
 export default router;
