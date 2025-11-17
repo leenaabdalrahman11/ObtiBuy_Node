0031b82a4fd013ee84bfd748599f546c75b1dc7f
leenaabdalrahman11 <leenasba02@gmail.com>
2025-11-17 15:14:55 +0200

    Fix email sending with Gmail SMTP

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,20 +1,22 @@
 import nodemailer from "nodemailer";
-export async function sendEmail(to,subject,html){
+
+export async function sendEmail(to, subject, html) {
     const transporter = nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-            user:process.env.SENDER_EMAIL,
-            pass:process.env.SENDER_PASS,
-        },
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.SENDER_EMAIL,
+            pass: process.env.SENDER_PASS, 
+          },
     });
+
     const info = await transporter.sendMail({
-        from:`"OptiBuy" <${process.env.SENDER_EMAIL}>`,
+        from: `"OptiBuy" <${process.env.SENDER_EMAIL}>`,
         to,
         subject,
         html,
     });
+
+    console.log("Email sent:", info.messageId);
 }
-
-
-
-
