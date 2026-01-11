a38f76264444e6746a48de18008093e212af7f9c
leenaabdalrahman11 <leenasba02@gmail.com>
2026-01-11 18:33:12 +0200

    fix:send email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,19 +2,48 @@ import nodemailer from "nodemailer";
 
 export async function sendEmail(to, subject, html) {
   const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT || 587),
     secure: false,
     auth: {
-      user: process.env.BREVO_SMTP_USER,
-      pass: process.env.BREVO_SMTP_PASS,
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
     },
   });
 
-  await transporter.sendMail({
-    from: `OptiBuy <${process.env.SENDER_EMAIL}>`,
+  const info = await transporter.sendMail({
+    from: `"OptiBuy" <${process.env.SENDER_EMAIL}>`,
     to,
     subject,
     html,
   });
+
+  return info.messageId;
 }
+
+
+
+
+/*import nodemailer from "nodemailer";
+
+export async function sendEmail(to, subject, html) {
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.SENDER_EMAIL,
+            pass: process.env.SENDER_PASS, 
+          },
+    });
+
+    const info = await transporter.sendMail({
+        from: `"OptiBuy" <${process.env.SENDER_EMAIL}>`,
+        to,
+        subject,
+        html,
+    });
+
+    console.log("Email sent:", info.messageId);
+}
+*/
