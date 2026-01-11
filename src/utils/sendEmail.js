248f9b6a6f476d3886a95f42092f508fee98570a
leenaabdalrahman11 <leenasba02@gmail.com>
2026-01-11 17:51:34 +0200

    fix: brevo email + confirm redirect

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,42 +1,24 @@
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.SENDER_EMAIL,
-    pass: process.env.SENDER_PASS,
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 15000,
-  tls: {
-    servername: "smtp.gmail.com",
-  },
-});
-
 export async function sendEmail(to, subject, html) {
-  try {
-    const sendPromise = transporter.sendMail({
-      from: `"OptiBuy" <${process.env.SENDER_EMAIL}>`,
-      to,
-      subject,
-      html,
-    });
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
 
-    const timeoutPromise = new Promise((_, reject) =>
-      setTimeout(() => reject(new Error("EMAIL_TIMEOUT")), 15000)
-    );
-
-    const info = await Promise.race([sendPromise, timeoutPromise]);
-    console.log("Email sent:", info.messageId);
-    return true;
-  } catch (err) {
-    console.log("Email failed:", err?.message || err);
-    return false;
-  }
+  await transporter.sendMail({
+    from: `"OptiBuy" <${process.env.SENDER_EMAIL}>`,
+    to,
+    subject,
+    html,
+  });
 }
+
 
 
 
