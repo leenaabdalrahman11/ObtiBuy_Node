fbf4ca3a46f2dd20cfe153e6cfb800b11b02aecd
leenaabdalrahman11 <leenasba02@gmail.com>
2026-05-02 09:19:18 +0300

    use brevo api for email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,29 +1,33 @@
-import nodemailer from "nodemailer";
+import axios from "axios";
 
 export async function sendEmail(to, subject, html) {
   try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
-      secure: false,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+    const res = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "OptiBuy",
+          email: process.env.SENDER_EMAIL,
+        },
+        to: [{ email: to }],
+        subject,
+        htmlContent: html,
       },
-    });
+      {
+        headers: {
+          "api-key": process.env.BREVO_API_KEY,
+          "content-type": "application/json",
+          accept: "application/json",
+        },
+      }
+    );
 
-    const info = await transporter.sendMail({
-      from: `"OptiBuy" <${process.env.SENDER_EMAIL}>`,
-      to,
-      subject,
-      html,
-    });
-
-    console.log("Email sent:", info.messageId);
-
-    return info;
+    console.log("BREVO RESPONSE:", res.data);
+    return res.data;
   } catch (error) {
-    console.log("EMAIL ERROR:", error);
+    console.log("BREVO ERROR STATUS:", error.response?.status);
+    console.log("BREVO ERROR DATA:", error.response?.data);
+    console.log("BREVO ERROR MESSAGE:", error.message);
     throw error;
   }
 }
